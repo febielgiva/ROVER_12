e599c3da728e7a612299584eb84b32ad5e54b44b
Kae Sawada <ksawada@calsatela.edu>
2016-04-13 15:03:36 -0700

    ROVER_12.java added. Just a copy of ROVER_00 besides the class name and constructor name

---
+++ src/swarmBots/ks_test.java
@@ -0,0 +1,14 @@
+package swarmBots;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ks_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
